5724d27c4a856eaaf743b9d74bd0cb8159c4ca4e
ianinakachan <ianinakachan@gmail.com>
2018-05-02 10:27:12 +0300

    first commit

---
+++ cv_form.js
@@ -0,0 +1,14 @@
+﻿const list = document.getElementById('skills');
+function on_add_skill_click() {
+    const input = document.getElementById('skill_field')
+    const new_skill = input.value;
+    if (new_skill === "") {
+        alert('Пустой навык? :-(');
+    }
+    else {
+        const entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(new_skill));
+        list.appendChild(entry);
+        input.value = ""
+    }
+}
