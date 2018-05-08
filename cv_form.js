8eea7183d3bd31b3415a09a4fe5704511c66c466
Ianina <ianinakachan@gmail.com>
2018-05-08 13:36:26 +0300

    improved cv_form
    - removed float everywhere
    - improved alignment, line-height
    - text of the criteria names is now differ from other text
    - message "blank skill" is now displayed next to the field

--- cv_form.js
+++ cv_form.js
@@ -1,11 +1,25 @@
 ﻿const list = document.getElementById('skills');
+
 function on_add_skill_click() {
     const input = document.getElementById('skill_field')
     const new_skill = input.value;
-    if (new_skill === "") {
-        alert('Пустой навык? :-(');
+    const parent = list.parentElement;
+    const messages = parent.getElementsByClassName('message');
+
+    if (new_skill.replace(/\s/g, "") === "") {
+
+        if (messages.length === 0) {
+            message = document.createElement('div');
+            message.setAttribute('class', 'message');
+            message.appendChild(document.createTextNode('Пустой навык? :-('));
+            parent.appendChild(message);
+        }
     }
     else {
+
+        if (messages.length > 0) {
+            parent.removeChild(messages[0])
+        }
         const entry = document.createElement('li');
         entry.appendChild(document.createTextNode(new_skill));
         list.appendChild(entry);
